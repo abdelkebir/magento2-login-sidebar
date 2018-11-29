ea913374692e04244eb6f76bbf6101c6f8ad9ff5
EL Harfali <elharfali.abdelkebir@gmail.com>
2018-11-29 05:46:27 +0100

    First Commit

---
+++ Block/Sidebar.php
@@ -0,0 +1,9 @@
+<?php
+namespace Godogi\LoginSidebar\Block;
+class Sidebar extends \Magento\Framework\View\Element\Template
+{
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+	{
+		parent::__construct($context);
+	}
+}
